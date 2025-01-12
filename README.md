0f766c61c54e0f7e9907aefb8db9320a6e570b97
Andrew Gallant <jamslam@gmail.com>
2025-01-12 14:40:37 -0500

    jiff: add some tweaks and docs

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-# Tauri + Vanilla
+This repository is meant to provide a means to minimally test [Jiff]'s Android
+support. Currently, this just involves printing out some logs and values without
+any assertions. It's pretty janky.
 
-This template should help get you started developing with Tauri in vanilla HTML, CSS and Javascript.
+To use this, the following steps are required:
 
-## Recommended IDE Setup
+* Get Android Studio set up. I did this by installing `android-ndk-25` from the
+Archlinux packages and then `android-studio`. Before running `android-studio`,
+I set `export ANDROID_NDK_HOME=/opt/android-ndk`.
+* Once Android Studio was open, I fumbled around its GUI to create a "device."
+I used a Pixel 9 Pro. I followed a bunch of dialogs that I didn't understand.
+Then I ran the device.
+* Then I ran `cargo tauri android dev` from the root of this repository. The
+first time will take a long while.
 
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+I'd also like to get Android testing in CI, and it seems like the best bet
+there is something like [cargo-dinghy](https://github.com/sonos/dinghy). I'm
+very concerned about doing this though because it relies on a lot of other
+external tooling to make it work (including spinning up an Android emulator).
+I'm already struggling to keep up with CI breaking because of some random
+WASM failure.
+
+[Jiff]: https://github.com/BurntSushi/jiff
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,21 +1,27 @@
 use jiff::{Timestamp, ToSpan};
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let time: Timestamp = "2024-07-11T01:14:00Z".parse().unwrap();
-    let zoned = time.intz("America/New_York").unwrap().checked_add(1.month().hours(2)).unwrap();
-    println!("actual: {}", zoned);
-    println!("expected: 2024-08-10T23:14:00-04:00[America/New_York]");
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("jiff=trace"))
+        .init();
 
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let time: Timestamp = "2024-07-11T01:14:00Z".parse().unwrap();
+    let zoned = time
+        .intz("America/New_York")
+        .unwrap()
+        .checked_add(1.month().hours(2))
+        .unwrap();
+    println!("##################### actual: {}", zoned);
+    println!("##################### expected: 2024-08-10T23:14:00-04:00[America/New_York]");
+    println!(
+        "##################### system: {:?}",
+        jiff::tz::TimeZone::system()
+    );
+
+    let now = jiff::Zoned::now();
+    println!("##################### now: {now}");
+    let rounded = now.round(jiff::Unit::Minute).unwrap();
+    println!("##################### now: {rounded}");
+
+    std::process::exit(0);
 }
